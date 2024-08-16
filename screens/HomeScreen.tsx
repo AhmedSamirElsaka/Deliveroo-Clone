40a3a30ae485e24b93d0bc7f1f5795154f8ccb15
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-08-16 19:02:35 +0300

    finish the header design

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -12,7 +12,7 @@ const HomeScreen = () => {
     });
   }, []);
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5 flex-1">
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           //   source={require("../assets/images/favicon.png")}
@@ -20,11 +20,14 @@ const HomeScreen = () => {
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
 
-        <View>
+        <View className="flex-1">
           <Text className="text-gray-400 font-bold text-xs">Deliver Now </Text>
-          <Text className="text-xl font-bold">Current location </Text>
-          <Icons.ChevronDownIcon size={20} color="#00CCBB" />
+          <Text className="text-xl font-bold">
+            Current location
+            <Icons.ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
+        <Icons.UserIcon size={35} color="#00CCBB" />
       </View>
     </SafeAreaView>
   );

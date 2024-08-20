a4dc37cb80089a59dccd3560574d770d022dba2f
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-08-20 08:12:33 +0300

    create and initialize the dish row component

--- screens/RestaurantScreen.tsx
+++ screens/RestaurantScreen.tsx
@@ -97,6 +97,17 @@ const RestaurantScreen = () => {
 
       <View>
         <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+        {dishes?.map((dish) => (
+          <DishRow
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+          />
+        ))}
       </View>
     </ScrollView>
   );
